968e8b706afa171f84d3afd9dfef00f51140c5e7
Chengenchong <145186137+Chengenchong@users.noreply.github.com>
2023-10-15 02:47:07 +0800

    yes

--- ethkl/src/App.js
+++ ethkl/src/App.js
@@ -24,21 +24,51 @@ function App() {
     const token = new ethers.Contract('');
   };
 
+
   return (
     <body>
-      <header>
-        <div className="header-content">
-          <h1 style={{ marginTop: '80px' }}>Welcome to DiffJob</h1>
-        </div>
-        <div className="logo-container" style={{ marginTop: '-70px' }}>
-          <img src={logo} alt="Rotating Image" className="rotating-image" />
-        </div>
-      </header>
-      <div className="content">
-        <p>This is the main content. You can add more content here.</p>
-        <p>Hello World!</p>
-        <button onClick={() => reqBal()}>Request Balance</button>
+    <div class="color-bar">
+      <button class="uiverse">
+      <div class="rapper">
+        <span>DIFFJOB</span>
+        <div class="circle circle-12"></div>
+        <div class="circle circle-11"></div>
+        <div class="circle circle-10"></div>
+        <div class="circle circle-9"></div>
+        <div class="circle circle-8"></div>
+        <div class="circle circle-7"></div>
+        <div class="circle circle-6"></div>
+        <div class="circle circle-5"></div>
+        <div class="circle circle-4"></div>
+        <div class="circle circle-3"></div>
+        <div class="circle circle-2"></div>
+        <div class="circle circle-1"></div>
       </div>
+      </button>
+      <button class="challengesButton">Challenge</button>
+      <button class="eventButton">Event</button>
+      <button class="jobButton">Job</button>
+
+    </div>
+    <div class="group">
+    <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
+        <g>
+            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+        </g>
+    </svg>
+      <input placeholder="Search" type="search" class="input"></input>
+    </div>
+    <div class="text">
+      <h1>ONGOING</h1>
+    </div>
+    <div class="card">
+    <div class="card-details">
+      <p class="text-title">Hackathon EthKL</p>
+      <p class="text-body">Competition for HACKERS</p>
+    </div>
+      <button class="card-button">Apply Now</button>
+    </div>
+
     </body>
   );
 }
